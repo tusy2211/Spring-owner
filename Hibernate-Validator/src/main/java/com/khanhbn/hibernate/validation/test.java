c81a29aa7ae247bfa8ed3aeff8e30b7e69b460c0
khanhbn <khanhbnd00052@fpt.edu.vn>
2020-07-07 09:12:37 +0700

    clean code

--- Hibernate-Validator/src/main/java/com/khanhbn/hibernate/validation/test.java
+++ Hibernate-Validator/src/main/java/com/khanhbn/hibernate/validation/test.java
@@ -12,10 +12,6 @@ public @interface test {
     // trường message là bắt buộc, khai báo nội dung sẽ trả về khi field k hợp lệ
     String message() default "url: must start with http://";
     // Cái này là bắt buộc phải có để Hibernate Validator có thể hoạt động
-//    Class<?>[] group() default {};
-    // Cái này là bắt buộc phải có để Hibernate Validator có thể hoạt động
-//    Class<? extends Payload>[] payload() default {};
-
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
 }
